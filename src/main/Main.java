c6465d8b3bbe111fec64fe7f70c97b390028c350
NicolasDaniel21 <nicolasdanielw13@gmail.com>
2024-04-25 18:03:58 -0300

    Adicionados diretórios dot e svg Files

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,27 +9,42 @@ public class Main {
         BinarySearchTree tree = new BinarySearchTree();
 
         // Inserindo elementos na árvore
-        tree.insert(50);
-        tree.insert(30);
-        tree.insert(70);
-        tree.insert(20);
-        tree.insert(40);
-        tree.insert(60);
-        tree.insert(80);
+        tree.insert(55);
+        tree.insert(25);
+        tree.insert(85);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(65);
+        tree.insert(95);
 
         // Imprimindo a árvore em ordem
         System.out.println("Arvore em ordem:");
         tree.inOrder();
         System.out.println();
 
-        // Buscando um valueor específico
+        // Buscando um valor específico
         int searchValue = 40;
         System.out.println("Buscando o valor " + searchValue + ": " + tree.search(searchValue));
 
         // Removendo um nodo
-        int removevalue = 30;
+        int removevalue = 25;
         tree.remove(removevalue);
-        System.out.println("Após remover " + removevalue + ":");
+        System.out.println("Após tentar remover " + removevalue + ":");
+        tree.printTree();
+
+        removevalue = 60;
+        tree.remove(removevalue);
+        System.out.println("Após tentar remover " + removevalue + ":");
+        tree.printTree();
+        
+        removevalue = 55;
+        tree.remove(removevalue);
+        System.out.println("Após tentar remover " + removevalue + ":");
+        tree.printTree();
+
+        int insertValue = 80;
+        tree.insert(insertValue);
+        System.out.println("Após tentar inserir " + insertValue + ":");
         tree.printTree();
 
         System.out.println("Em ordem novamente: ");
